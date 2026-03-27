498c621890dfaf00ec44dd2ac1dac75ba790877c
cesar <zs24016729@estudiantes.uv.mx>
2026-03-27 15:28:48 -0600

    calculadora

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,54 @@
+class Input {
+    public String entrada(String[] args){
+
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Usage: java Calculator operand1 operator operand2");
+        }else{
+            try {
+                Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("Entrada Incorrecta: " + args[0]);
+            }
+
+            try {
+                Integer.parseInt(args[2]);
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("Entrada Incorrecta: " + args[2]);
+            }
+        }
+        
+        int resultado = 0;
+
+        switch (args[1].charAt(0)) {
+            case '+':
+                resultado = Integer.parseInt(args[0]) + Integer.parseInt(args[2]);
+                break;
+            case '-':
+                resultado = Integer.parseInt(args[0]) - Integer.parseInt(args[2]);
+                break;
+            case '.':
+                resultado = Integer.parseInt(args[0]) * Integer.parseInt(args[2]);
+                break;
+            case '/':
+                resultado = Integer.parseInt(args[0]) / Integer.parseInt(args[2]);
+                break;
+        }
+
+        return ((args[0] + ' ' + args[1] + ' ' + args[2] + " = " + resultado));
+
+    }
+}
+
+public class Calculadora {
+    public static void main(String[] args) { 
+
+        try {
+            Input entrada = new Input();
+            System.out.println(entrada.entrada(args));
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
